12334cad6bbe1d4626db7d531c341c6ffaeeeb6e
Valentina Gómez <valengogu96@gmail.com>
2025-05-06 18:07:23 +0000

    terminamos proyecto

--- src/app.js
+++ src/app.js
@@ -1,26 +1,22 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 window.onload = function() {
   let randomWho= who[Math,floor(Math.random()* who.length)];
   let randomAction = action[Math,floor(Math.random()* action.length)];
   let randomWhat = what[Math.floor(Math.random()* what.length)];
   let randomWhen = when[Math,floor(Math,random()* when.length)];
-
   let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
   document.getElementById("excuse").innerText = excuse;
 };
 
-let numeros = [1,2,3,4]
+  let numeros = [1,2,3,4]
 
 console.log(Math.floor * numeros.length)
